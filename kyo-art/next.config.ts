b88add726e1005ec9e85ff6f2cbfb71e3cbca3a2
fredmunene <mfredgitonga@gmail.com>
2025-11-27 13:10:06 +0300

    add image

--- kyo-art/next.config.ts
+++ kyo-art/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["http://192.168.1.31:3000"]
+  allowedDevOrigins: ["http://192.168.1.31:3000", "http://localhost:3000", "http://127.0.0.1:3000","https://64672429cd16.ngrok-free.app","kyoart.vercel.app"]
 };
 
 export default nextConfig;
